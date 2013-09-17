1215e395c660f292aceb0e88dc6b0998b69284a0
Mark Pittillo <mark-pittillo@uiowa.edu>
2013-09-16 20:21:25 -0500

    New routes for about and links

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class StaticPagesTest < ActionDispatch::IntegrationTest
   test "Access about page" do
-    get "static_pages/about"
+    get "/about"
     assert_response :success
     assert_select("h1", "About")
   end
 
   test "Access links page" do
-    get "static_pages/links"
+    get "links"
     assert_response :success
     assert_select("h1", "Links")
   end
