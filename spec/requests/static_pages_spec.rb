0c34709ab59b5f7edbec119f352a08f3af887805
Mark Pittillo <mark-pittillo@uiowa.edu>
2014-02-21 16:30:45 -0600

    Tests for static pages and user pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,19 +2,35 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "About page" do
+  subject { page }
 
-    it "should have the content 'About'" do
-      visit 'about'
-      expect(page).to have_content('About')
-    end
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Links page" do
 
-    it "should have the content 'Links'" do
-      visit '/links'
-      expect(page).to have_content('Links')
-    end
+    before { visit links_path }
+    let(:heading) { 'Links' }
+    let(:page_title) { 'Link' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Links"
+    expect(page).to have_title(full_title('Links'))
   end
 end
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "User pages" do
+  1 == 1
+end
